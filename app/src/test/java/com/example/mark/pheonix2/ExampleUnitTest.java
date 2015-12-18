9f7f4526090797b036c9feb9f90e295a216d08b2
xyzMark <dieckman7461@gmail.com>
2015-12-18 15:14:30 -0800

    delted split branch amending new line here

--- app/src/test/java/com/example/mark/pheonix2/ExampleUnitTest.java
+++ app/src/test/java/com/example/mark/pheonix2/ExampleUnitTest.java
@@ -14,6 +14,8 @@ public class ExampleUnitTest extends InstrumentationTestCase{
     // first commit of the master branch (Intention of merge conflicts)
     // This is the first commit of the SplitBranch
     // one more for the road
+    // deleted split branch
+    // testing amending a commit
 
 
 
