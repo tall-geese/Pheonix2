477f94fe20b32bace0aaf911357448b494921681
xyzMark <dieckman7461@gmail.com>
2016-01-31 20:56:36 -0800

    Got the viewpager working with the materialtabs. Work on importing functionality into the new activity before mergin back to the main thread.

--- app/src/main/java/com/example/mark/pheonix2/NewMainActivity.java
+++ app/src/main/java/com/example/mark/pheonix2/NewMainActivity.java
@@ -1,15 +1,31 @@
 package com.example.mark.pheonix2;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.karim.MaterialTabs;
 
 /**
  * Created by Mark on 1/26/2016.
  */
 public class NewMainActivity extends AppCompatActivity{
+    private static final int NUM_PAGES = 3;
+    private static final String AppTag = "AppTag";
+    private ViewPager mPager;
+    private MaterialTabs mTabs;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +33,35 @@ public class NewMainActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new ScreenSlidePagerAdapter(getSupportFragmentManager()));
+        mTabs = (MaterialTabs) findViewById(R.id.materialTabs);
+        mTabs.setViewPager(mPager);
+
     }
+private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+    private final String[] tabTitles = {"One", "Two", "Three", "Four", "Five", "Six", "Seven"};
+
+    public ScreenSlidePagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_PAGES;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new PagerFragment();
+    }
+}
 
 
     @Override
@@ -25,6 +69,13 @@ public class NewMainActivity extends AppCompatActivity{
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // we can override this method if we want to set back the page instead
+        // of closing the activity altogether
+        super.onBackPressed();
     }
 
     @Override
--- app/src/main/java/com/example/mark/pheonix2/PagerFragment.java
+++ app/src/main/java/com/example/mark/pheonix2/PagerFragment.java
@@ -1,6 +1,6 @@
 package com.example.mark.pheonix2;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 public class PagerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        //this may be incorrect, might need to cast as a viewgroup here then return that
         return inflater.inflate(R.layout.fragment_content, container, false);
 
     }
