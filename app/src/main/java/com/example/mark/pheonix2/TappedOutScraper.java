8d1f43f356a76c37ed582000c1896e2171bad0b4
xyzMark <dieckman7461@gmail.com>
2016-02-09 10:54:40 -0800

    nearly finished user-deck search method. work on adding redundant elements to our list

--- app/src/main/java/com/example/mark/pheonix2/TappedOutScraper.java
+++ app/src/main/java/com/example/mark/pheonix2/TappedOutScraper.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import info.debatty.java.stringsimilarity.Levenshtein;
+
 /**
  * Created by Mark on 2/7/2016.
  */
@@ -26,6 +28,7 @@ public class TappedOutScraper {
     }
 
     public ArrayList<Element> getDeckSearchResults(ArrayList<String> searchQuery){
+        //TODO: replace arraylist searchquery with array instead, get rid of conc. for loop, replace with stringBuilder
         ArrayList<Element> arrayList = new ArrayList<>();
 
         String urlLeading = ctx.getResources().getString(R.string.tappedoutNoUserURLQueryLeading);
@@ -52,9 +55,33 @@ public class TappedOutScraper {
         return new ArrayList<Element>();
     }
 
-    public ArrayList<Elements> getDeckSearchResults(ArrayList<String> searchQuery, boolean userDefinedFlag, String userName){
+    public ArrayList<Element> getDeckSearchResults(String[] splitSearchQuery, boolean userDefinedFlag, String userName){
         // check for the userDefinedFlag == true
         // this will handle differently from the sister method, we are scraping decks from the user web page
         // but we will also need to sort the list and return results that best matches to the searchQuery
+
+        ArrayList<Element> arrayList = new ArrayList<>();
+
+        try{
+            Document doc = Jsoup.connect("http://tappedout.net/users/" + userName).get();
+            Elements elements = doc.select("h4.name name-long > a[title]");
+            for(Element e : elements){
+                for(String s : e.text().split(" ")){
+                    for(String q : splitSearchQuery){
+                        Levenshtein l = new Levenshtein();
+                        double distance = (s.length() < q.length()) ?
+                                l.distance(s, q.substring(0, s.length()-1)) :
+                                l.distance(q, s.substring(0, q.length()-1));
+                        if (distance < 2)
+                            arrayList.add(e); //then do something to move onto the next element
+                        // or maybe check to see  if the arrayList already has that element in it //
+                    }
+                }
+            }
+            //return the arrayList here, maybe return empty one if we cant find a better soltion to that
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
